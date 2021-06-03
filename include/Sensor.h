1f9e1096c7e176a07820fbb698722e019070f774
Feeee23 <63659321+Feeee23@users.noreply.github.com>
2021-06-03 11:38:06 +0200

    Update Sensor.h

--- include/Sensor.h
+++ include/Sensor.h
@@ -1,25 +1,17 @@
 #include <Arduino.h>
+#include<SR04.h>
 
 int Trig=5; //Entspricht D1
 int Echo=4; //Entspricht D2 
-int Dauer;
 
-int Distance(){ //komunikation zum Sensor
-    digitalWrite(Trig, LOW);
-    delayMicroseconds(5);
-    digitalWrite(Trig, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(Trig, LOW);
-    Dauer = pulseIn(Echo, HIGH);
-    return Dauer*0.034/2;
-}
+SR04 sr04=SR04(Echo, Trig);
 
 int Sensor(){                            //Schuckt den Sensor an
-    int A = Distance(); //Messe dreimal hintereinander um Feler zu minimiern
-    int B = Distance();
-    int C = Distance();
+    int A = sr04.Distance(); //Messe dreimal hintereinander um Feler zu minimiern
+    int B = sr04.Distance();
+    int C = sr04.Distance();
     int D = (A + B + C) / 3; //Durchschnitt der 3 Werte ermitteln
-    int F = 190 - D; //Die Höhe des Sensors (240cm) minus den Messwert ergibt den Füllstand
+    int F = 200 - D; //Die Höhe des Sensors (200cm) minus den Messwert ergibt den Füllstand
     if(F<0){
         F=0;
     }
@@ -31,8 +23,8 @@ int Rechner(int FS){
     if (FS<140){ //under 1,4m ist die Zisterne Zylindrisch
         l=100*3.1415*FS/10;
     }else{ //Darueber Kegelstumpf
-    l=4400+(FS/10-14)*314.15-36.65*(FS/10-14)*(FS/10-14)+1.43*(FS/10-14)*(FS/10-14)*(FS/10-14); //max wert Zylindrsich plus Zylinderstumpf formel (r2 durch h ausgedrueckt) !!!Achtung Rundungen auf Dezimeter verfälschen das Ergebniss
-    //l=4400+((FS-140)*31415-366.5*(FS-140)*(FS-140)+1.43*(FS-140)*(FS-140)*(FS-140))/1000; // genauer?
+    //l=4400+(FS/10-14)*314.15-36.65*(FS/10-14)*(FS/10-14)+1.43*(FS/10-14)*(FS/10-14)*(FS/10-14); //max wert Zylindrsich plus Zylinderstumpf formel (r2 durch h ausgedrueckt) !!!Achtung Rundungen auf Dezimeter verfälschen das Ergebniss
+    l=4400+((FS-140)*31415-366.5*(FS-140)*(FS-140)+1.43*(FS-140)*(FS-140)*(FS-140))/1000; // genauer?
     }
     return (int) l; //Literanzahl zurueckgeben                            //rechnet die Liter aus
 }
